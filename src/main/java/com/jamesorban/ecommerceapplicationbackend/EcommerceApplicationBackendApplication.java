2905e337504ebabd9dd3ea7f43ce30925f502294
orbanjames <96356809+orbanjames@users.noreply.github.com>
2022-11-05 09:44:25 +0100

    first commit

---
+++ src/main/java/com/jamesorban/ecommerceapplicationbackend/EcommerceApplicationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jamesorban.ecommerceapplicationbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceApplicationBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceApplicationBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jamesorban/ecommerceapplicationbackend/EcommerceApplicationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamesorban.ecommerceapplicationbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
